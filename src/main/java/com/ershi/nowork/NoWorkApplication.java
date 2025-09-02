3e3c7228b609a3a141270cf486fea234ca63ad0b
yunxiang.gu <yunxiang.gu@hand-china.com>
2025-09-02 09:42:46 +0800

    [init]:项目初始化

---
+++ src/main/java/com/ershi/nowork/NoWorkApplication.java
@@ -0,0 +1,13 @@
+package com.ershi.nowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ershi/nowork/NoWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ershi.nowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

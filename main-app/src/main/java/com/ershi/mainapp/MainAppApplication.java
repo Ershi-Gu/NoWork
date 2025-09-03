1e472e90e19f798708fda68428fac6b75e940f23
Ershi <2402554312@qq.com>
2025-09-03 17:19:32 +0800

    [fix]:修复common模块没有扫描到的问题
    (cherry picked from commit 00a2da80efb7e1593a6c2a4cafa2384e0334ba80)

--- main-app/src/main/java/com/ershi/mainapp/MainAppApplication.java
+++ main-app/src/main/java/com/ershi/mainapp/MainAppApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 项目主启动类
@@ -12,12 +14,15 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * @since 2025-09-02
  */
 @SpringBootApplication(scanBasePackages = {
-        "com.ershi.hotboard"
+        "com.ershi.common",
+        "com.ershi.hotboard",
 })
 @MapperScan(value = {
         "com.ershi.hotboard.mapper"
 })
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAsync
+@EnableScheduling
 public class MainAppApplication {
 
 

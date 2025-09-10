d84a7933a5267010479040e2a3e454ad657f95a1
Ershi <2402554312@qq.com>
2025-09-10 15:15:28 +0800

    [fix]:修复sa-token cookie名字包含冒号导致的无法读取问题，添加跨源设置

--- common/src/main/java/com/ershi/common/config/InterceptorConfig.java
+++ common/src/main/java/com/ershi/common/config/InterceptorConfig.java
@@ -1,8 +1,10 @@
 package com.ershi.common.config;
 
+import cn.dev33.satoken.interceptor.SaInterceptor;
 import com.ershi.common.interceptor.CollectorInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,7 +21,24 @@ public class InterceptorConfig implements WebMvcConfigurer {
     private CollectorInterceptor collectorInterceptor;
 
     /**
+     * cookie和跨域配置
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // 允许携带 Cookie
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
+    /**
      * 添加拦截器
+     *
      * @param registry
      */
     @Override
@@ -27,5 +46,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
         // 信息收集拦截器
         registry.addInterceptor(collectorInterceptor)
                 .addPathPatterns("/**");
+        // 注册 Sa-Token 拦截器，打开注解式鉴权功能
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
     }
 }
